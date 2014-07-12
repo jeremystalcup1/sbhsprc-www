af1435c8a3981aa4699b03c53e0b4ef15741454e
jeremystalcup1 <jeremystalcup@gmail.com>
2014-07-12 14:42:39 -0400

    Fixed marquee

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-$('.rainbow').rainbow({ 
+$('.rainbow').each(function() {
+  $(this).rainbow({ 
     colors: [
-        '#FF0000',
-        '#f26522',
-        '#fff200',
-        '#00a651',
-        '#28abe2',
-        '#2e3192',
-        '#6868ff'
+      '#FF0000',
+      '#f26522',
+      '#fff200',
+      '#00a651',
+      '#28abe2',
+      '#2e3192',
+      '#6868ff'
     ],
     animate: true,
     animateInterval: 100,
     pad: false,
     pauseLength: 100,
+  })
 });
