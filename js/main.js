7fa92161eb3737fcc312b823afa911b8c45ea6ff
jeremystalcup1 <jeremystalcup@gmail.com>
2014-07-12 14:33:24 -0400

    Added js Magic. And changed subheader

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$('.rainbow').rainbow({ 
+    colors: [
+        '#FF0000',
+        '#f26522',
+        '#fff200',
+        '#00a651',
+        '#28abe2',
+        '#2e3192',
+        '#6868ff'
+    ],
+    animate: true,
+    animateInterval: 100,
+    pad: false,
+    pauseLength: 100,
+});
