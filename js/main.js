b5438ebf795b5902d3620b35cc9220a62b9b1c78
Stuart Olivera <stuart@stuartolivera.com>
2014-08-11 01:26:48 -0400

    Moved javascript into a closure

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
-$('.rainbow').each(function() {
-  $(this).rainbow({ 
-    colors: [
-      '#FF0000',
-      '#f26522',
-      '#fff200',
-      '#00a651',
-      '#28abe2',
-      '#2e3192',
-      '#6868ff'
-    ],
-    animate: true,
-    animateInterval: 100,
-    pad: false,
-    pauseLength: 100,
-  })
-});
+(function() {
+
+  $('.rainbow').each(function() {
+    $(this).rainbow({
+      colors: [
+        '#FF0000',
+        '#f26522',
+        '#fff200',
+        '#00a651',
+        '#28abe2',
+        '#2e3192',
+        '#6868ff'
+      ],
+      animate: true,
+      animateInterval: 100,
+      pad: false,
+      pauseLength: 100,
+    })
+  });
+
+}).call(this);
